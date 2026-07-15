c05d6481d86758d336c9704299a709646e6c1e86
Overlandranger <36721943+Overlandranger@users.noreply.github.com>
2026-07-15 09:22:19 -0600

    Expose Ghost Car panel API

--- velvet_interface/__init__.py
+++ velvet_interface/__init__.py
@@ -5,12 +5,9 @@ __version__ = "0.2.0"
 
 from velvet_interface.core import Scene, Surface, Widget, Router
 from velvet_interface.route_request import SceneRouteRequest, build_scene_route_request
+from velvet_interface.core.ghost_can_panel import render_ghost_can_text, view_model_from_ghost_can_event
 
 __all__ = [
-    "Scene",
-    "Surface",
-    "Widget",
-    "Router",
-    "SceneRouteRequest",
-    "build_scene_route_request",
+    "Scene", "Surface", "Widget", "Router", "SceneRouteRequest",
+    "build_scene_route_request", "render_ghost_can_text", "view_model_from_ghost_can_event",
 ]
