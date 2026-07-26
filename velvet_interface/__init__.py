6fc06792213721398f35e85fc8fc88929c669d8d
Overlandranger <36721943+Overlandranger@users.noreply.github.com>
2026-07-25 20:36:21 -0700

    Export InterfaceLifecycle from package root

--- velvet_interface/__init__.py
+++ velvet_interface/__init__.py
@@ -4,10 +4,12 @@
 __version__ = "0.2.0"
 
 from velvet_interface.core import Scene, Surface, Widget, Router
+from velvet_interface.lifecycle import InterfaceLifecycle
 from velvet_interface.route_request import SceneRouteRequest, build_scene_route_request
 from velvet_interface.core.ghost_can_panel import render_ghost_can_text, view_model_from_ghost_can_event
 
 __all__ = [
-    "Scene", "Surface", "Widget", "Router", "SceneRouteRequest",
-    "build_scene_route_request", "render_ghost_can_text", "view_model_from_ghost_can_event",
+    "Scene", "Surface", "Widget", "Router", "InterfaceLifecycle",
+    "SceneRouteRequest", "build_scene_route_request", "render_ghost_can_text",
+    "view_model_from_ghost_can_event",
 ]
